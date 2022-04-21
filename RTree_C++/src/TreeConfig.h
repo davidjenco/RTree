f54db938cae4e8e72222581c38b902342a8f8e02
jencodav <jencodav@fit.cvut.cz>
2022-04-21 20:35:54 +0200

    TreeConfig node size changed - still needs workaround

--- RTree_C++/src/TreeConfig.h
+++ RTree_C++/src/TreeConfig.h
@@ -9,8 +9,9 @@ struct TreeConfig {
     std::string treeFileName = "../tree";
     int metadataOffset = 16;
     uint32_t numberOfNodes = 0;
-    uint32_t nodeSizeInBytes = 1025;
     uint32_t minNodeEntries = 1; //TODO nastavit pak dynamicky podle dimenze a toho maxu
+    uint32_t maxPossibleNodeSize = 1265;
+    uint32_t nodeSizeInBytes = 1265; //TODO takhle nemůžu určit node size in bytes --> rozbije se s různými dimenzemi
     uint32_t maxNodeEntries;
     uint32_t maxLeafNodeEntries;
     uint32_t dimension;
