ef474c81f5db1fa63000cb5dd2fdc66e795c89be
jencodav <jencodav@fit.cvut.cz>
2022-04-21 20:25:03 +0200

    Serializing routing entry fix (one fking line)

--- RTree_C++/src/RoutingEntry.cpp
+++ RTree_C++/src/RoutingEntry.cpp
@@ -8,7 +8,6 @@ RoutingEntry::RoutingEntry(const DataRow &dataRow) : childNodeId(dataRow.rowId),
 
 void RoutingEntry::serializeEntry(fstream &treeFileStream, bool inLeafNode) {
     treeFileStream.write((char *) & childNodeId, sizeof(childNodeId));
-    cout << "Serializing entry with id " << childNodeId << endl;
     for (size_t j = 0; j < from.size(); ++j) {
         treeFileStream.write((char *) & from[j], sizeof(from[j]));
         if (!inLeafNode){
